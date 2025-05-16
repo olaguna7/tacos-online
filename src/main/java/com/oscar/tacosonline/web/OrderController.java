bea80ba011919984bda46f52e18696ac6a3d8e8c
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-16 13:18:54 +0200

    Add order model and controller

---
+++ src/main/java/com/oscar/tacosonline/web/OrderController.java
@@ -0,0 +1,27 @@
+package com.oscar.tacosonline.web;
+
+import com.oscar.tacosonline.model.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping({"/orders", "/orders/"})
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order) {
+        log.info("Order Processing Started: " + order);
+        return "redirect:/";
+    }
+}
