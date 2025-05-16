3e7a9b04b2e21fa85988997b232e575adba39084
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-16 09:52:11 +0200

    First commit

---
+++ src/main/java/com/oscar/tacosonline/TacosOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.tacosonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacosOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacosOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oscar/tacosonline/TacosOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.tacosonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacosOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
