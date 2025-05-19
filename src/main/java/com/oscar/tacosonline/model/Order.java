905d6b95bcf9947ea6008224692f10a4cb8fd6d3
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-19 09:26:54 +0200

    add Taco and Order models

--- src/main/java/com/oscar/tacosonline/model/Order.java
+++ src/main/java/com/oscar/tacosonline/model/Order.java
@@ -1,15 +1,28 @@
 package com.oscar.tacosonline.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class Order {
+    @NotBlank(message = "El nombre del taquito es obligatorio")
     private String name;
+    @NotBlank(message = "El nombre de la calle es obligatoria")
     private String street;
+    @NotBlank(message = "El nombre de la ciudad es obligatoria")
     private String city;
+    @NotBlank(message = "El nombre del estado es obligatorio")
     private String state;
+    @NotBlank(message = "El nombre del zip es obligatorio")
     private String zip;
+    @CreditCardNumber(message = "No te hagás el vivo, que esta tarjeta es falsa")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])(\\/)([2-9][0-9])", message = "La fecha debe ser MM/AA")
     private String ccExpiration;
+    @Digits(integer = 3, fraction = 0, message = "CVV inválido")
     private String ccCVV;
 }
--- src/main/java/com/oscar/tacosonline/model/Taco.java
+++ src/main/java/com/oscar/tacosonline/model/Taco.java
@@ -2,10 +2,14 @@ package com.oscar.tacosonline.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min = 5, message = "El nombre del taquito tiene que tener como mínimo 5 caracteres")
     private String name;
     private List<String> ingredients;
 }
