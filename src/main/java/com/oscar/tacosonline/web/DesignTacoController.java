e0f3faa5d593cbf47b21e80042529ec0b778a03d
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-19 11:28:33 +0200

    updated design taco and order controllers

--- src/main/java/com/oscar/tacosonline/web/DesignTacoController.java
+++ src/main/java/com/oscar/tacosonline/web/DesignTacoController.java
@@ -2,15 +2,13 @@ package com.oscar.tacosonline.web;
 
 import com.oscar.tacosonline.model.Ingredient;
 import com.oscar.tacosonline.model.Ingredient.Type;
+import com.oscar.tacosonline.model.Order;
 import com.oscar.tacosonline.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Arrays;
@@ -20,13 +18,26 @@ import java.util.stream.Collectors;
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm(Model model) {
         fillIngredients(model);
-        model.addAttribute("tktn", new Taco());
+        // model.addAttribute("tktn", new Taco());
         return "design";
+    }
+
+    @ModelAttribute(name = "tktn")
+    public Taco taco() {
+        log.info("Iniciando taco");
+        return new Taco();
+    }
+
+    @ModelAttribute
+    public Order order() {
+        log.info("Iniciando orden");
+        return new Order();
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
@@ -36,12 +47,13 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(@Valid @ModelAttribute(name="tktn") Taco design, Errors errors, Model model) {
+    public String processDesign(@Valid @ModelAttribute(name="tktn") Taco design, Errors errors, Model model, @ModelAttribute Order order) {
         if (errors.hasErrors()) {
             fillIngredients(model);
             return "design";
         }
         log.info("Processing Design Taco: " + design);
+        order.addDesign(design);
         return "redirect:/orders/current";
     }
 
--- src/main/java/com/oscar/tacosonline/web/OrderController.java
+++ src/main/java/com/oscar/tacosonline/web/OrderController.java
@@ -8,17 +8,18 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping({"/orders", "/orders/"})
+@SessionAttributes("order")
 public class OrderController {
 
     @GetMapping("/current")
     public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
         return "orderForm";
     }
 
