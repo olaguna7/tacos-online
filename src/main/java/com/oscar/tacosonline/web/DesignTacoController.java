450a114bffa7c4a2a324d17ef4e34b25a6ed4073
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-19 09:27:11 +0200

    add controllers for taco and order

--- src/main/java/com/oscar/tacosonline/web/DesignTacoController.java
+++ src/main/java/com/oscar/tacosonline/web/DesignTacoController.java
@@ -6,10 +6,13 @@ import com.oscar.tacosonline.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +24,28 @@ public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm(Model model) {
+        fillIngredients(model);
+        model.addAttribute("tktn", new Taco());
+        return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(ingredient -> ingredient.getType() == type).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute(name="tktn") Taco design, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            fillIngredients(model);
+            return "design";
+        }
+        log.info("Processing Design Taco: " + design);
+        return "redirect:/orders/current";
+    }
+
+    public void fillIngredients(Model model){
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
@@ -35,24 +60,9 @@ public class DesignTacoController {
         );
 
         Ingredient.Type[] types = Ingredient.Type.values();
-        for (Ingredient.Type type : types) {
+
+        for(Ingredient.Type type : types){
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
-
-        model.addAttribute("tktn", new Taco());
-
-        return "design";
-    }
-
-    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-        return ingredients
-                .stream()
-                .filter(ingredient -> ingredient.getType() == type).collect(Collectors.toList());
-    }
-
-    @PostMapping
-    public String processDesign(Taco design, Model model) {
-        log.info("Processing Design Taco: " + design);
-        return "redirect:/orders/current";
     }
 }
