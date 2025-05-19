01f5264fff44796f57e3b1e151bbab81881028ac
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-19 11:29:22 +0200

    Add WebConfig.java to eliminate HomeController with no logic

--- src/main/java/com/oscar/tacosonline/web/HomeController.java
+++ src/main/java/com/oscar/tacosonline/web/HomeController.java
@@ -3,11 +3,11 @@ package com.oscar.tacosonline.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
-
-    @GetMapping({"/","/home", "/home/"})
-    public String showHomePage() {
-        return "home";
-    }
-}
+//@Controller
+//public class HomeController {
+//
+//    @GetMapping("/")
+//    public String showHomePage() {
+//        return "home";
+//    }
+//}
